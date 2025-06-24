3038a63f43a5d1807184c15d489d70b12a4233c4
Hem1144 <hemlaldulal7@gmail.com>
2025-06-24 18:27:49 +0100

    feat: Initialize the api project

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+    
+    return data
+
+@app.get("/")
+def patient():
+    return {'message':'Patients management system'}
+
+
+@app.get("/about")
+def about():
+    return {'message': 'API for manage patients records'}
+
+
+@app.get('/view')
+def view():
+    data = load_data()
+    
+    return data
+
+
